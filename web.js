c563aa61717c7fcb7aa2a43be44778e194096a79
Karl Svartholm <gorbiz@gmail.com>
2013-01-13 12:15:03 +0100

    mongo installed and working

--- web.js
+++ web.js
@@ -1,5 +1,17 @@
 var express = require('express');
 
+var mongo = require('mongodb');
+var mongoUri = process.env.MONGOLAB_URI ||
+  'mongodb://localhost/mydb';
+
+mongo.Db.connect(mongoUri, function (err, db) {
+  db.collection('mydocs', function(er, collection) {
+    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+      console.log ("IT WORKS....I think")
+    });
+  });
+});
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
