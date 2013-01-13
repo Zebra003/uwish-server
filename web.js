230b7bef7b4aef58a23d830605d93f461607d4fa
Karl Svartholm <gorbiz@gmail.com>
2013-01-13 12:27:50 +0100

    mongo test

--- web.js
+++ web.js
@@ -4,18 +4,18 @@ var mongo = require('mongodb');
 var mongoUri = process.env.MONGOLAB_URI ||
   'mongodb://localhost/mydb';
 
-mongo.Db.connect(mongoUri, function (err, db) {
-  db.collection('mydocs', function(er, collection) {
-    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
-      console.log ("IT WORKS....I think")
-    });
-  });
-});
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+
+  mongo.Db.connect(mongoUri, function (err, db) {
+    db.collection('mydocs', function(er, collection) {
+      collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+        response.send("IT WORKS....I think")
+      });
+    });
+  });
 });
 
 var port = process.env.PORT || 5000;
